b2ec8191fc6d4be96bd363b46fafd2bc52847382
juanc970410 <juan.c.herrerav@hotmail.com>
2018-02-21 07:21:18 -0500

    done activity details

--- src/app/pages/activity-details/activity-details.component.ts
+++ src/app/pages/activity-details/activity-details.component.ts
@@ -16,7 +16,7 @@ export class ActivityDetailsComponent implements OnInit{
   private activityForm: FormGroup;
   private activity = new Activity("Climbing Monserrate", "In this activity we'll be going all the way up to Monserrate by foot",
    new User("juanc","Juan","juan.herrera@prueba.com","Herrera","password"),
-   ["Spanish","English"], "Monserrate", "20/02/2018", [new User("Carlos","Carlos","carlos@prueba.com","Sanchez","password"),
+   ["Spanish","English"], "Monserrate", "20/02/2018 10:00 am", [new User("Carlos","Carlos","carlos@prueba.com","Sanchez","password"),
    new User("jhordy","Jhordy","jhordy@prueba.com","Salinas","password")],"40000");
 
   constructor(
@@ -27,14 +27,14 @@ export class ActivityDetailsComponent implements OnInit{
 
   ngOnInit(){
     this.activityForm = this.formBuilder.group({
-      name:this.activity.name,
-      description:this.activity.description,
-      administrator: this.activity.administrator,
-      languages: this.activity.languages,
-      location: this.activity.location,
-      date: this.activity.date,
-      participants: this.activity.participants,
-      price: this.activity.price
+      name:'',
+      description:'',
+      administrator:'',
+      languages:'',
+      location:'',
+      date:'',
+      participants:'',
+      price:''
   });
   }
 }
